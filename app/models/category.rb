90eb853ea576f21b2af188874ee960c620de7414
Sam <samuelrush@gmail.com>
2019-12-12 22:38:39 +0000

    Refactoring

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@ class Category < ActiveRecord::Base
   has_many :products
 
   def count_products
-    @count_products = Product.where(category_id: id).count
+    Product.where(category_id: id).count
   end
 
 end
